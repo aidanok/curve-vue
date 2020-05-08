517cd633051a8f12d2134337d1f15832b1cfc890
aidanok <52333119+aidanok@users.noreply.github.com>
2020-05-08 13:57:23 +0100

    set vue publicPath to .

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,7 @@ const HtmlCriticalWebpackPlugin = require("html-critical-webpack-plugin");
 const path = require('path')
 
 module.exports = {
+	publicPath: process.env.NODE_ENV === 'production'? '.': '/',
 	chainWebpack: config => {
 		config.plugins.delete('prefetch')
 		config.module.rule('worker')
